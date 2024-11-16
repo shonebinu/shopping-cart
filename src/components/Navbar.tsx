acb12d4e9a3f79f7b95e8a918efd807ff256008c
Shone Binu <62597277+shonebinu@users.noreply.github.com>
2024-11-16 21:05:31 +0530

    feat: active link highlight

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,17 +2,24 @@ import { NavLink } from "react-router-dom";
 import { Button } from "@/components/ui/button";
 
 function Navbar() {
-  // TODO: Navbar isActive state to highlight the active nav link
   return (
     <nav className="flex justify-between items-center py-2 px-8">
-      <div className="font-semibold underline">Fakestore</div>
+      <div className="font-semibold underline">fakestore</div>
       <div className="flex">
-        <Button asChild variant="link">
-          <NavLink to="/">Home</NavLink>
-        </Button>
-        <Button asChild variant="link">
-          <NavLink to="/store">Store</NavLink>
-        </Button>
+        <NavLink to="/">
+          {({ isActive }) => (
+            <Button variant="link" className={isActive ? "text-blue-500" : ""}>
+              Home
+            </Button>
+          )}
+        </NavLink>
+        <NavLink to="/store">
+          {({ isActive }) => (
+            <Button variant="link" className={isActive ? "text-blue-500" : ""}>
+              Store
+            </Button>
+          )}
+        </NavLink>
       </div>
     </nav>
   );
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,14 +4,16 @@ import { Link } from "react-router-dom";
 function Home() {
   return (
     <section className="flex flex-col justify-center items-center gap-5 h-[90%]">
-      <h1 className="text-3xl font-bold">Welcome to Fake Store</h1>
+      <h1 className="text-3xl font-bold">
+        Welcome to <span className="underline">Fake Store</span>
+      </h1>
       <p className="lg:w-[40vw] text-center">
         Want a fake Bugatti? Clothes? Electronics? Anything you want, all within
         your reach. Check it out now!
       </p>
-      <Button asChild>
-        <Link to="/store">Visit Store</Link>
-      </Button>
+      <Link to="/store">
+        <Button>Visit Store</Button>
+      </Link>
     </section>
   );
 }
