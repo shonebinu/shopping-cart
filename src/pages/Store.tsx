b8634da8371e3e5c0e0df87a9f0443f7e4a20203
Shone Binu <62597277+shonebinu@users.noreply.github.com>
2024-11-16 21:47:20 +0530

    chore: init product cards

--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -1,4 +1,20 @@
 import { useQuery } from "@tanstack/react-query";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
+type Product = {
+  id: number;
+  title: "string";
+  category: "string";
+  image: "string";
+  price: number;
+};
 
 function Store() {
   const { isPending, error, data } = useQuery({
@@ -6,8 +22,33 @@ function Store() {
     queryFn: () =>
       fetch("https://fakestoreapi.com/products").then((res) => res.json()),
   });
+  console.log(data);
 
-  return <section>{JSON.stringify(data)}</section>;
+  return (
+    <section className="flex flex-col items-center p-8">
+      <div className="grid grid-cols-3 gap-3 w-[60%]">
+        {data &&
+          data.map((product: Product) => (
+            <Card className="rounded-sm" key={product.id}>
+              <CardHeader>
+                <CardTitle>
+                  <p className="leading-5">{product.title}</p>
+                </CardTitle>
+                <CardDescription className="text-blue-500">
+                  {product.category}
+                </CardDescription>
+              </CardHeader>
+              <CardContent>
+                <img src={product.image} alt={product.title} className="w-5" />
+              </CardContent>
+              <CardFooter>
+                <p>${product.price}</p>
+              </CardFooter>
+            </Card>
+          ))}
+      </div>
+    </section>
+  );
 }
 
 export default Store;
