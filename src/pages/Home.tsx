ad39575e80bea29230fdac5b40d88ec9277617cf
Shone Binu <62597277+shonebinu@users.noreply.github.com>
2024-11-16 21:26:19 +0530

    fix: make company name nowrap

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,10 +4,11 @@ import { Link } from "react-router-dom";
 function Home() {
   return (
     <section className="flex flex-col justify-center items-center gap-5 h-[90%]">
-      <h1 className="text-3xl font-bold">
-        Welcome to <span className="underline">Fake Store</span>
+      <h1 className="text-3xl font-bold text-center">
+        Welcome to{" "}
+        <span className="underline whitespace-nowrap">Fake Store</span>
       </h1>
-      <p className="lg:w-[40vw] text-center">
+      <p className="lg:w-[40vw] text-center px-2">
         Want a fake Bugatti? Clothes? Electronics? Anything you want, all within
         your reach. Check it out now!
       </p>
